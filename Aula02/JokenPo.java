0ca63c5eec4ec3cd09bdf7950801ab6801ce5be5
unknown <caroliny.franca@fatec.sp.gov.br>
2023-07-12 21:45:54 -0300

    JokenPo Update

--- Aula02/JokenPo.java
+++ Aula02/JokenPo.java
@@ -2,7 +2,6 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class JokenPo {
-
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         Random random = new Random();
